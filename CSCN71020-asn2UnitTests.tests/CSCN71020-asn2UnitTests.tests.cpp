40507acf516a1049b64f0efa71cabc846fe7318e
inderrajsaini <inderraj.r.saini27@gmail.com>
2021-10-16 22:05:22 +0530

    Test for getArea()

--- CSCN71020-asn2UnitTests.tests/CSCN71020-asn2UnitTests.tests.cpp
+++ CSCN71020-asn2UnitTests.tests/CSCN71020-asn2UnitTests.tests.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 extern "C" int getPerimeter(int* length, int* width);
+extern "C" int getArea(int* length, int* width);
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -24,5 +25,23 @@ namespace CSCN71020asn2UnitTeststests
 			Assert::AreEqual(EXPECTEDPERIMETER, p);
 
 		}
+
+
+		TEST_METHOD(findArea)
+		{
+			//This test is used to check the value of area of a rectangle
+			int l = 5;//Value of length
+			int w = 4;//Value of width
+
+			const int EXPECTEDAREA = 20;
+			int p;
+
+			p = getArea(&l, &w);
+
+			Assert::AreEqual(EXPECTEDAREA, p);
+
+		}
+
+
 	};
 }
