e728ee2df50d6a553c4bc7abde6bf11af9ceb924
inderrajsaini <inderraj.r.saini27@gmail.com>
2021-10-16 22:16:38 +0530

    tests for setLength()

--- CSCN71020-asn2UnitTests.tests/CSCN71020-asn2UnitTests.tests.cpp
+++ CSCN71020-asn2UnitTests.tests/CSCN71020-asn2UnitTests.tests.cpp
@@ -2,6 +2,8 @@
 #include "CppUnitTest.h"
 extern "C" int getPerimeter(int* length, int* width);
 extern "C" int getArea(int* length, int* width);
+extern "C" int setWidth(int input, int* width);
+extern "C" int setLength(int input, int* length);
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -42,6 +44,39 @@ namespace CSCN71020asn2UnitTeststests
 
 		}
 
+
+		TEST_METHOD(TestingLength001)
+		{
+			//This test is used to check the value of perimeter of a rectangle
+			int length;//Value of length
+			int input = 1;
+
+			setLength(input, &length);
+
+			Assert::AreEqual(length, input);
+		}
+		TEST_METHOD(TestingLength002)
+		{
+			//This test is used to check the value of perimeter of a rectangle
+			int length;//Value of length
+			int input = 99;
+
+			setLength(input, &length);
+
+			Assert::AreEqual(length, input);
+
+		}
+		TEST_METHOD(TestingLength003)
+		{
+			//This test is used to check the value of perimeter of a rectangle
+			int length;//Value of length
+			int input = 100;
+
+			setLength(input, &length);
+
+			Assert::AreNotEqual(length, input);
+
+		}
 
 	};
 }
